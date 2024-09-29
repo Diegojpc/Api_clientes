65586b9bcff3ace3eb20f41210d35c8e4449c139
Diego <diegojosepc3@hotmail.com>
2024-09-29 18:32:45 -0500

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+pymysql://root:Diego_jose123@localhost/gamma_clientes')
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Diego_jose123@localhost:3306/gamma_clientes'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
